192ebb9bfe2ce7aaa51f592becb9956ac4c4a9e6
GreggWhitman <32632196+GreggWhitman@users.noreply.github.com>
2019-11-17 16:57:47 +0100

    refactoring

--- src/utils.js
+++ src/utils.js
@@ -17,47 +17,49 @@ export const calcCursorPosition = (x, y) => [
   y - window.innerHeight / 2,
 ]
 
+const translate3D = (x, y) => `translate3d(${x}px,${y}px,0)`
+
 export const Planetoids = [
   {
     key: '1',
     color: 'red',
-    translate: (x, y) => `translate3d(${x / 2 + 25}px,${y / 2 - 160}px,0)`,
+    translate: (x, y) => translate3D(x / 2 + 25, y / 2 - 160),
     size: 15,
   },
   {
     key: '2',
     color: 'cornflowerblue',
-    translate: (x, y) => `translate3d(${x / 3.2 - 80}px,${y / 3.2 + 95}px,0)`,
+    translate: (x, y) => translate3D(x / 3.2 - 80, y / 3.2 + 95),
     size: 25,
   },
   {
     key: '3',
     color: 'darksalmon',
-    translate: (x, y) => `translate3d(${x / 3.9 + 269}px,${y / 3.9 - 362}px,0)`,
+    translate: (x, y) => translate3D(x / 3.9 + 269, y / 3.9 - 362),
     size: 30,
   },
   {
     key: '4',
     color: 'burlywood',
-    translate: (x, y) => `translate3d(${x / 4.7 - 320}px,${y / 4.7 + 240}px,0)`,
+    translate: (x, y) => translate3D(x / 4.7 - 320, y / 4.7 + 240),
     size: 40,
   },
   {
     key: '5',
     color: 'brown',
-    translate: (x, y) => `translate3d(${x / 5.1 + 220}px,${y / 5.1 + 160}px,0)`,
+    translate: (x, y) => translate3D(x / 5.1 + 220, y / 5.1 + 160),
     size: 55,
   },
   {
     key: '6',
     color: 'mediumslateblue',
-    translate: (x, y) => `translate3d(${x / 5.8 + 395}px,${y / 5.8 - 26}px,0)`,
+    translate: (x, y) => translate3D(x / 5.8 + 395, y / 5.8 - 26),
     size: 60,
   },
   {
     key: '7',
     color: 'plum',
-    translate: (x, y) => `translate3d(${x / 6.2 - 600}px,${y / 6.2 - 123}px,0)`,
+    translate: (x, y) => translate3D(x / 6.2 - 600, y / 6.2 - 123),
     size: 70,
   },
 ]
